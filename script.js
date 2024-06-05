9aaf23cab8eb38ac85c7bbcbe57a930b936c578d
Ronaldo Monteiro Da Silva Junior <00001098706249sp@aluno.educacao.sp.gov.br>
2024-06-05 14:42:08 -0300

    imc

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function calcular(){
-    var fpeso = parseFloat(document.getElementById("peso"))
-    var faltura = parseFloat(document.getElementById("altura"))
-    var fIMC = fpeso/(faltura*fpeso)
+
+    var fPeso = parseFloat(document.getElementById("peso").value)
+
+    console.log(fPeso)
+    var fAltura = parseFloat(document.getElementById("altura").value)
+    var fimc = fPeso/(fAltura*fAltura)
+    document.getElementById("imc").innerHTML = fimc.toFixed(2)
+    if(fimc<18.5){
+        document.getElementById("staus").innerHTML = "STATUS: BAIXO PESO"
+    }else if(fimc<=24.9){
+        document.getElementById("staus").innerHTML = "STATUS: PESO IDEAL"
+    }else if(fimc<=29.9){
+        document.getElementById("staus").innerHTML = "STATUS: SOBREPESO"
+    }else if(fimc<=34.9){
+        document.getElementById("staus").innerHTML = "STATUS: OBESIDADE 1"
+    }else if(fimc<=39.9){
+        document.getElementById("staus").innerHTML = "STATUS:  OBESIDADE 2"
+    }else{
+        document.getElementById("staus").innerHTML = "STATUS:  OBESIDADE MORBIDA"
+    }
 }
